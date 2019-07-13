d1fef0ca99a98353ff105438142decc5fbb1f06a
Brentton Neander <b.neander@vieinteractive.com>
2019-07-13 14:25:59 -0500

    Created Forms and Utils module to accommodate for code reuseability across entire app.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,124 @@
+from django.conf import settings
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    # User,
+)
+from django.core.validators import RegexValidator
 from django.db import models
+# from datetime import datetime
+# from django import forms
+from django.core.mail import send_mail
+from django.db.models.signals import post_save
+from .utils import code_generator
 
-# Create your models here.
+USERNAME_REGEX = '^[a-zA-Z0-9.@+-]*$'
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            username,
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    username = models.CharField(max_length=255,
+                                validators=[
+                                    RegexValidator(
+                                        regex=USERNAME_REGEX,
+                                        message='Username must be Alphanumeric or'
+                                                ' contain any of the following: ". @ + -"',
+                                        code='invalid_username'
+                                    )],
+                                unique=True,
+                                )
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=120, default='60406')
+    tel = models.CharField(max_length=15, null='888-888-8888', blank='888-888-8888', default='888-888-8888')
+    street_address = models.CharField(max_length=300, null='Street Address', blank='Street Address',
+                                      default='Street Address')
+    city = models.CharField(max_length=200, null='City', blank='City', default='City')
+    state = models.CharField(max_length=200, null='State', blank='State', default='State')
+    picture = models.ImageField(upload_to='photos/accounts/%Y/')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=300, null=True, blank=True)
+    tel = models.CharField(max_length=15, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    state = models.CharField(max_length=200, null=True, blank=True)
+    zip_code = models.CharField(max_length=200, null=True, blank=True)
+    picture = models.ImageField(default='profile_img.jpg', upload_to='photos/accounts/%Y/')
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user.username)
+
+
+def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        try:
+            Profile.objects.create(user=instance)
+            # ActivationProfile.objects.create(user=instance)
+        except:
+            pass
+
+
+post_save.connect(post_save_user_model_receiver, sender=settings.AUTH_USER_MODEL)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,186 @@
-from django.shortcuts import render
+from django.contrib import (
+    messages,
+    auth,
+)
+from django.contrib.auth import (
+    login,
+    get_user_model,
+    logout,
+)
+from django.shortcuts import (
+    render,
+    redirect,
+)
+from django.contrib.auth.forms import (
+    UserChangeForm,
+    PasswordChangeForm,
+)
+from django.contrib.auth import update_session_auth_hash
+from django.views.generic import View
+from django.views.generic.base import (
+    TemplateView,
+    TemplateResponseMixin,
+    ContextMixin,
+)
 
-# Create your views here.
+from django.core.paginator import (
+    EmptyPage,
+    PageNotAnInteger,
+    Paginator,
+)
+from .forms import (
+    UserCreationForm,
+    UserLoginForm,
+    EditProfileForm,
+    # SendMessageForm,
+    # RegistrationForm,
+)
+
+from django.http import (
+    HttpResponseRedirect,
+    HttpResponse,
+)
+from .models import Profile
+# from django.contrib.auth.models import User
+# from datetime import datetime
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import (
+    login_required,
+    permission_required,
+)
+User = get_user_model()
+
+
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **kwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**kwargs)
+        return login_required(view)
+
+
+class ProfileView(LoginRequiredMixin, ContextMixin, TemplateResponseMixin, View):
+    template_name = 'accounts/dashboard.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['title'] = 'some title'
+        return self.render_to_response(context)
+
+
+class HelpView(LoginRequiredMixin, ContextMixin, TemplateResponseMixin, View):
+    template_name = 'accounts/help.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['supportTickets'] = Support.objects.all
+        return self.render_to_response(context)
+
+
+@login_required(login_url='/accounts/login/')
+def AdminView(request, *args, **kwargs):
+    inquiry = Inquiry.objects.order_by('-contact_date')
+    paginator = Paginator(inquiry, 10)
+    page = request.GET.get('page')
+    paged_inquiries = paginator.get_page(page)
+
+    enrollment = Enrollment.objects.order_by('-contact_date')
+    pagination = Paginator(enrollment, 10)
+    paged_enrollments = pagination.get_page(page)
+
+    tour = Tour.objects.order_by('-contact_date')
+    paginate = Paginator(tour, 10)
+    paged_tours = paginate.get_page(page)
+
+    context = {
+        'client_leads': paged_inquiries,
+        'client_enrollments': paged_enrollments,
+        'client_tours': paged_tours,
+    }
+
+    return render(request, 'accounts/admin.html', context, *args, **kwargs)
+
+
+@login_required(login_url='/accounts/login/')
+def SupportView(request, *args, **kwargs):
+    support_ticket = Support.objects.order_by('-id').filter(status=True)
+    paginator = Paginator(support_ticket, 10)
+    page = request.GET.get('page')
+    paged_tickets = paginator.get_page(page)
+    closed_ticket = Support.objects.order_by('-id').filter(status=False)
+    ct_paginator = Paginator(closed_ticket, 10)
+    page = request.GET.get('page')
+    paged_clostedtickets = ct_paginator.get_page(page)
+
+    context = {
+        'support_ticket': paged_tickets,
+        'closed_ticket': paged_clostedtickets,
+    }
+    return render(request, 'accounts/support.html', context, *args, **kwargs)
+
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/edit_dashboard.html', args)
+
+
+@login_required(login_url='/accounts/login/')
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'Your password has been updated!')
+            return redirect('profile')
+        else:
+            return redirect('accounts/change_password.html')
+
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+        args = {'form': form}
+        return render(request, 'accounts/change_password.html', args)
+
+
+def register(request, *args, **kwargs):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'You are now registered and can log in')
+        return redirect('login')
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request, *args, **kwargs):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username_ = form.cleaned_data.get('username')
+        user_obj = User.objects.get(username__iexact=username_)
+        login(request, user_obj)
+        messages.success(request, 'You are now logged in')
+        return redirect('profile')
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# def send_message(request, *args, **kwargs):
+#     form = BaseWriteForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         messages.success(request, 'Your message has been sent!')
+#         return redirect('profile')
