22f5b764c353f47f34d6f03e19ef335d93f54e44
Brentton Neander <b.neander@vieinteractive.com>
2019-07-16 15:06:19 -0500

    Updated template with schema block. Updated Blog urls file to display detail and list view.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'blog'
+
+urlpatterns = [
+    path('', views.list_view, name='list'),
+    path('<slug:slug>/', views.detail_view, name='detail'),
+]
